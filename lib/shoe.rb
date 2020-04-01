4294dec74cafd807c3bc1b6b3d5829033750b385
Brandon Yoon <byoon5397@gmail.com>
2020-04-01 14:09:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
-# Make your shoe class here!
+class shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def condition
+    puts "The shoe has been repaired."
+  end
+end  
